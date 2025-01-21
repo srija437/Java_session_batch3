1544e1f371775fd3af562b14d4c16afa4ccf5f5c
srija437 <2311cs010009@mallareddyuniversity.ac.in>
2025-01-21 16:24:52 +0530

    Add files via upload

---
+++ Solution.java
@@ -0,0 +1,24 @@
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int p1 = m-1;
+        int p2 = n-1;
+        int i = nums1.length -1;
+
+        while(p1 >= 0 && p2 >= 0){
+            if(nums1[p2] > nums1[p1]) {
+                nums1[i] = nums2[p2];
+                p2--;
+                i--;
+            } else {
+                nums1[i] = nums1[p1];
+                 p1--;
+                i--;
+            }
+            while(p2 >=0){
+                nums1[i] = nums[p2];
+                p2--;
+                i--;
+            }
+        }
+    }
+}
