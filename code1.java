0531b43bfe53cb28f23cbb175c3f37ea0b7c3b5e
srija437 <2311cs010009@mallareddyuniversity.ac.in>
2025-01-22 12:41:08 +0530

    Add files via upload

---
+++ code1.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+
+     int uIdx = 0;
+
+     for(int i = 1; i < nums.length; i++) {
+        if(nums[i] != nums[uIdx]) {
+            nums[uIdx+1] = nums[i];
+            uIdx++;
+        }
+     }   
+     return uIdx+1;
+    }
+}
